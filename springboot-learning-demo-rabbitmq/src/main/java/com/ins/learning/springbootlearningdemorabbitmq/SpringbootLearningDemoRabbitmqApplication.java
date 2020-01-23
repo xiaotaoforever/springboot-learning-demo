fc1678bd5a55056db9f733e6d69e4a0302339921
wangtao <wangtao@instrument.com.cn>
2020-01-23 20:38:19 +0800

    rabbitmq demo first commit！

---
+++ springboot-learning-demo-rabbitmq/src/main/java/com/ins/learning/springbootlearningdemorabbitmq/SpringbootLearningDemoRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.ins.learning.springbootlearningdemorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningDemoRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningDemoRabbitmqApplication.class, args);
+	}
+
+}
---
+++ springboot-learning-demo-rabbitmq/src/test/java/com/ins/learning/springbootlearningdemorabbitmq/SpringbootLearningDemoRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ins.learning.springbootlearningdemorabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningDemoRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
